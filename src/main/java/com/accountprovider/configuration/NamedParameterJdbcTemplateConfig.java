5d751be47642d2319a9a17fdd378bb4cc1ecbb24
Mihai <dimamihaimail@gmail.com>
2023-01-31 21:05:09 +0200

    sadsada

--- src/main/java/com/accountprovider/configuration/NamedParameterJdbcTemplateConfig.java
+++ src/main/java/com/accountprovider/configuration/NamedParameterJdbcTemplateConfig.java
@@ -1,7 +1,6 @@
 package com.accountprovider.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
--- src/main/java/com/accountprovider/controller/HelloController.java
+++ src/main/java/com/accountprovider/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,10 +20,14 @@ public class HelloController {
         this.namedParameterJdbcTemplate = jdbcTemplate;
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/")
-    public ResponseEntity<String> hello() {
-        namedParameterJdbcTemplate.update("CREATE TABLE test(id int)", new MapSqlParameterSource());
-        return ResponseEntity.ok("Hello");
+    @RequestMapping(method = RequestMethod.PUT, path = "/{name}/create")
+    public ResponseEntity<String> hello(
+        @PathVariable String name
+    ) {
+        namedParameterJdbcTemplate.update("CREATE TABLE IF NOT EXISTS User(name VARCHAR(64))", new MapSqlParameterSource());
+        namedParameterJdbcTemplate.update("INSERT INTO User (name) VALUES (:name)",
+                new MapSqlParameterSource().addValue("name", name));
+        return ResponseEntity.ok(name);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
